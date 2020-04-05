496fc5e342c83b5baab49084080a48dc67823dae
yvangoufack <62734984+yvangoufack@users.noreply.github.com>
2020-04-06 01:27:18 +0200

    Update esercizio-C-2020-04-06.c

--- src/esercizio-C-2020-04-06.c
+++ src/esercizio-C-2020-04-06.c
@@ -1,8 +1,57 @@
 /*
- * esercizio-C-2020-04-06.c
- *
- *  Created on: Apr 2, 2020
- *      Author: marco
+ ============================================================================
+ Name        : Esercizio
+ Author      : Yvan Ngoufack
+ Version     :
+ Copyright   : Your copyright notice
+ Description : esercizio-C-2020-04-06
+ ============================================================================
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+	int FindBruteForce(int b[]){
+
+		int *ptr, i , x;
+
+		ptr=b;
+
+		int y = calloc(10, sizeof(int)); //alloco una dimensione per poter utilizzarla nel ciclo for
+
+		for (i=0;i<sizeof(int) ;i++){
+
+		  x= b[i];
+
+		}
+			 if (x!=*ptr)
+
+			 {
+
+				 printf("c'è un elemento non duplicato");
+			 }
+			 else {
+
+				 printf("tutto ok");
+			 }
+
+
+
+		}
+
+
+
+		int main(int argc, char *argv[]) {
+
+
+			int a []  = { 1,5,6,2,1,6,4,3,2,5,3};
+
+			FindBruteForce(a);
+
+		}
+
 
 
